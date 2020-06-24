48ddc3f396126f4264fce61c0edeb10f6fd13082
Rohit Kumar <Rohitkumar919@users.noreply.github.com>
2020-06-24 22:40:27 +0530

    Linked List completed

--- src/LInkedList/LinkedList.java
+++ src/LInkedList/LinkedList.java
@@ -21,7 +21,7 @@ public class LinkedList {
 		System.out.println("-----------------------------------");
 	}
 
-	public void addlast(int item) {
+	public void addLast(int item) {
 		// Create new node
 		Node nn = new Node();
 		nn.data = item;
@@ -49,20 +49,127 @@ public class LinkedList {
 		} else {
 			nn.next = this.head;
 		}
-		this.head = nn;	
+		this.head = nn;
 		this.size++;
 	}
+
 	public int getFirst() throws Exception {
-		if(this.size == 0) {
+		if (this.size == 0) {
 			throw new Exception("List is empty");
 		}
 		return this.head.data;
 	}
+
 	public int getLast() throws Exception {
-		if(this.size == 0) {
+		if (this.size == 0) {
 			throw new Exception("List is empty");
 		}
 		return this.tail.data;
 	}
-}
 
+	public int getAt(int index) throws Exception {
+		if (this.size == 0) {
+			throw new Exception("List is empty");
+		}
+		if (this.size < index || this.size < 0) {
+			throw new Exception("Invalid Index");
+		}
+		Node temp = head;
+		for (int i = 1; i <= index; i++) {
+			temp = temp.next;
+		}
+		return temp.data;
+	}
+
+	private Node getNodeAt(int index) throws Exception {
+		if (this.size == 0) {
+			throw new Exception("List is empty");
+		}
+		if (this.size < index || this.size < 0) {
+			throw new Exception("Invalid Index");
+		}
+		Node temp = head;
+		for (int i = 1; i <= index; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+
+	public void addAt(int item, int index) throws Exception {
+		if (this.size < index || index < 0) {
+			throw new Exception("Invalid Index");
+		}
+		if (index == 0) {
+			addFirst(item);
+		} else if (index == this.size) {
+			addLast(item);
+		} else {
+			// Create node
+			Node nn = new Node();
+			nn.data = item;
+			nn.next = null;
+
+			// Attach and Summary Update
+			Node nm = getNodeAt(index - 1);
+			Node np = getNodeAt(index);
+			nm.next = nn;
+			nn.next = np;
+		}
+
+	}
+
+	public int removeFirst() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("List is empty");
+		}
+		int rv = this.head.data;
+		if (this.size == 1) {
+			this.head = null;
+			this.tail = null;
+			this.size = 0;
+		} else {
+			this.head = this.head.next;
+			this.size--;
+		}
+		return rv;
+	}
+
+	public int removeLast() throws Exception {
+		if (this.size == 0) {
+			throw new Exception("List is empty");
+		}
+		int rv = this.tail.data;
+		if (this.size == 1) {
+			this.head = null;
+			this.tail = null;
+			this.size = 0;
+		} else {
+			this.tail = getNodeAt(this.size - 2);
+			this.tail.next = null;
+			this.size--;
+		}
+		return rv;
+	}
+
+	public int removeAt(int index) throws Exception {
+		if (this.size == 0) {
+			throw new Exception("List is empty");
+		}
+		if (this.size < index || index < 0) {
+			throw new Exception("Invalid Index");
+		}
+		if (index == 0) {
+			return removeFirst();
+		} else if (index == this.size - 1) {
+			return removeLast();
+		} else {
+			Node nm = getNodeAt(index - 1);
+			Node nn = nm.next;
+			Node np = nn.next;
+			nm.next = np;
+			this.size --;
+			return  nn.data;
+		}
+	}
+
+}
--- src/LInkedList/LinkedListClient.java
+++ src/LInkedList/LinkedListClient.java
@@ -4,15 +4,20 @@ public class LinkedListClient {
 
 	public static void main(String[] args) throws Exception {
 		LinkedList list1 = new LinkedList();
-		list1.addlast(10);
-		list1.addlast(20);
-		list1.addlast(30);
-		list1.addlast(40);
+		list1.addLast(10);
+		list1.addLast(20);
+		list1.addLast(30);
+		list1.addLast(40);
 		list1.addFirst(50);
 		list1.addFirst(60);
+		list1.addAt(60, 5);
+		list1.addAt(70, 0);
+		list1.addAt(80, 7);
+		list1.addAt(80, 8);
 		list1.display();
 		System.out.println(list1.getFirst());
 		System.out.println(list1.getLast());
+		System.out.println(list1.getAt(3));
 	}
 
 }
